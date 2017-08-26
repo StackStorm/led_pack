3fe9a867f17bade06c9db6dd6f19dd0bb5aaa72f
Dmitri <dz@stackstorm.com>
2017-08-26 09:00:01 -0700

    Make lint agreeable

--- sensors/switch_sensor.py
+++ sensors/switch_sensor.py
@@ -22,22 +22,21 @@ class SwitchSensor(PollingSensor):
             payload=data
         )
 
-
     def poll(self):
         switch_request = requests.get('http://192.168.33.10:8080/switches/')
         switch_data = switch_request.json()
 
-	if switch_data['is_on'] and switch_data['change']:
+        if switch_data['is_on'] and switch_data['change']:
             self._trigger(switch_data)
 
-	elif not self._prev_on and switch_data['is_on']:
+        elif not self._prev_on and switch_data['is_on']:
             switch_data['red'] = 100
             switch_data['green'] = 100
             switch_data['blue'] = 100
 
             self._trigger(switch_data)
 
-	elif self._prev_on and not switch_data['is_on']:
+        elif self._prev_on and not switch_data['is_on']:
             switch_data['red'] = 0
             switch_data['green'] = 0
             switch_data['blue'] = 0
--- sensors/test_sensor.py
+++ sensors/test_sensor.py
@@ -1,5 +1,3 @@
-import requests
-
 from st2reactor.sensor.base import PollingSensor
 
 __all__ = [
@@ -10,8 +8,8 @@ __all__ = [
 class TestSensor(PollingSensor):
     def __init__(self, sensor_service, config=None, poll_interval=None):
         super(TestSensor, self).__init__(sensor_service=sensor_service,
-                                           config=config,
-                                           poll_interval=poll_interval)
+                                         config=config,
+                                         poll_interval=poll_interval)
         self._kvp_trigger = 'led_pack.kvp_change'
         self._logger = self._sensor_service.get_logger(__name__)
 
@@ -21,10 +19,9 @@ class TestSensor(PollingSensor):
             payload=data
         )
 
-
     def poll(self):
         self._logger.info('Ran once')
-	kvp_value = self._sensor_service.get_value('kvp_test.kvp', local=False)
+        kvp_value = self._sensor_service.get_value('kvp_test.kvp', local=False)
         if kvp_value:
             self._logger.info('KVP Value: %s' % (kvp_value))
             data = {
